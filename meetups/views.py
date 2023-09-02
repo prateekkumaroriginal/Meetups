bfc7c92f6267b09642db9c70fe91196d0fb530db
Prateek <prateek.kumar.original@gmail.com>
2023-09-02 14:26:02 +0530

    Create Index page and style it

--- meetups/views.py
+++ meetups/views.py
@@ -2,6 +2,23 @@ from django.shortcuts import render
 from django.views import View
 
 # Create your views here.
+
+
 class IndexView(View):
     def get(self, request):
-        return render(request, 'meetups/index.html')
+        meetups = [
+            {
+                'title': 'A first Meetup',
+                'location': 'Mumbai',
+                'slug': 'a-first-meetup'
+            },
+            {
+                'title': 'Second meetup',
+                'location': 'Delhi',
+                'slug': 'second-meetup'
+            }
+        ]
+        return render(request, 'meetups/index.html', {
+            'meetups': meetups,
+            'show_meetups': True
+        })
